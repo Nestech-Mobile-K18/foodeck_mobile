bb704d6ea5a0b211ab58dc075ecbdd42afc944ec
DESKTOP-7EN38LH\LeQuocDat <leods7ven@gmail.com>
2024-04-10 20:58:12 +0700

    no message

--- lib/app.dart
+++ lib/app.dart
@@ -1,29 +1,41 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:template/resources/fonts.dart';
-import 'package:template/resources/routes.dart';
-import 'package:template/routes/router.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
+import 'package:get/get.dart';
+import 'package:template/pages/home/home_page.dart';
+import 'package:template/pages/login/login_page.dart';
+import 'package:template/pages/login/widgets/create_account.dart';
+import 'package:template/pages/login/widgets/login_email.dart';
+import 'package:template/pages/splash/splash_page.dart';
+import 'package:template/values/route.dart';
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My App',
-      theme: ThemeData(
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: Fonts.inter,
-        appBarTheme: const AppBarTheme(color: Colors.transparent),
-      ),
-      onGenerateRoute: generateRoute,
-      initialRoute: RouteName.splash,
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return FlutterSizer(
+      builder: (context, orientation, screenType) {
+        return GetMaterialApp(
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            // theme: Provider.of<ThemeProvider>(context).themeData,
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            defaultTransition: Transition.rightToLeft,
+            transitionDuration: const Duration(milliseconds: 600),
+            debugShowCheckedModeBanner: false,
+            initialRoute: splashPage,
+            routes: {
+              splashPage: (context) => const SplashPage(),
+              createAccount: (context) => const CreateAccount(),
+              loginEmail: (context) => const LoginEmail(),
+              loginPage: (context) => const LoginPage(),
+              homePage: (context) => const HomePage()
+            });
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_sizer/flutter_sizer.dart';
-import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:template/app.dart';
 import 'package:template/models/desktop_food.dart';
 import 'package:template/models/restaurant.dart';
-import 'package:template/pages/home/home_page.dart';
-import 'package:template/pages/login/login_page.dart';
-import 'package:template/pages/login/widgets/create_account.dart';
-import 'package:template/pages/login/widgets/login_email.dart';
-import 'package:template/pages/splash/splash_page.dart';
 import 'package:template/themes/theme_provider.dart';
-import 'package:template/values/route.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
@@ -43,32 +35,6 @@ Future<void> main() async {
       ), // Wrap your app
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    return FlutterSizer(
-      builder: (context, orientation, screenType) {
-        return GetMaterialApp(
-            theme: Provider.of<ThemeProvider>(context).themeData,
-            defaultTransition: Transition.rightToLeft,
-            transitionDuration: const Duration(milliseconds: 600),
-            debugShowCheckedModeBanner: false,
-            initialRoute: splashPage,
-            routes: {
-              splashPage: (context) => const SplashPage(),
-              createAccount: (context) => const CreateAccount(),
-              loginEmail: (context) => const LoginEmail(),
-              loginPage: (context) => const LoginPage(),
-              homePage: (context) => const HomePage()
-            });
-      },
-    );
-  }
 }
 
 final supabase = Supabase.instance.client;
